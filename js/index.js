2b1a8be8cb7b9a05e98a9dba0d82b59f9369a468
shannon-nabors <shannon.nabors@gmail.com>
2019-02-20 11:25:37 -0500

    Done.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,46 @@
 // using this file is optional
 // you can also load the code snippets in using your browser's console
+
+const main = document.getElementById('main')
+
+main.addEventListener('click', function(event) {
+  alert('I was clicked!')
+})
+
+
+const input = document.querySelector('input')
+
+// input.addEventListener('keydown', function(event) {
+//   console.log(event.key)
+// })
+
+input.addEventListener('keydown', function(event) {
+  if (event.key === "g") {
+    return event.preventDefault()
+  } else {
+    console.log(event.key)
+  }
+})
+
+let divs = document.querySelectorAll('div')
+
+// function bubble(event) {
+//   console.log(this.firstChild.nodeValue.trim() + ' bubbled')
+// }
+
+for (let i = 0; i < divs.length; i++) {
+  divs[i].addEventListener('click', bubble)
+}
+
+// function capture(event) {
+//   console.log(this.firstChild.nodeValue.trim() + ' captured')
+// }
+//
+// for (let i = 0; i < divs.length; i++) {
+//   divs[i].addEventListener('click', capture, true)
+// }
+
+function bubble(event) {
+  event.stopPropagation()
+  console.log(this.firstChild.nodeValue.trim() + ' bubbled')
+}
